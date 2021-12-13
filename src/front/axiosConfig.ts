d24930beda80c783d2f90985716fc877092f80ff
bmangust <ftf.pollux@gmail.com>
2021-12-13 19:56:53 +0300

    update env and connecting containers

--- src/front/axiosConfig.ts
+++ src/front/axiosConfig.ts
@@ -18,10 +18,24 @@ const prefix =
     ? process.env.NEXT_PUBLIC_BACKEND_PREFIX
     : '';
 
+const ssrHost =
+  process.env.NODE_ENV === 'production'
+    ? `${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}`
+    : 'localhost:2222';
+
 const baseURL = `${protocol}://${host}${prefix}/`;
-console.log({ baseURL });
+const ssrURL = `http://${ssrHost}/`;
+console.log({ baseURL, ssrURL });
+
 export const api = axios.create({
   baseURL,
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
+  validateStatus: (status) => status >= 200 && status < 500,
+});
+
+export const ssrApi = axios.create({
+  baseURL: ssrURL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
   validateStatus: (status) => status >= 200 && status < 500,
@@ -38,6 +52,19 @@ export const makePostRequest = async <T>(
   data: any,
 ): Promise<T> => {
   return api
+    .post(url, data)
+    .then((data) => data.data)
+    .catch((e) => console.log(e));
+};
+
+export const ssrGetRequest = async <T>(url: string): Promise<T> => {
+  return ssrApi
+    .get(url)
+    .then((data) => data.data)
+    .catch((e) => console.log(e));
+};
+export const ssrPostRequest = async <T>(url: string, data: any): Promise<T> => {
+  return ssrApi
     .post(url, data)
     .then((data) => data.data)
     .catch((e) => console.log(e));
